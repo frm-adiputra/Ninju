7fdc319a08289087e50c2bc08c443ba3981e75cc
Firmansyah Adiputra <frm.adiputra@gmail.com>
2017-04-21 14:30:17 +0700

    Initial commit

---
+++ example/tasks.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+sourcedir = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(sourcedir, '../src'))
+
+from ninju import Ninju, O
+
+n = Ninju()
+
+# all directories are relative to $ROOT
+tmp = n.dir('tmp')
+data_panitia_PTN1 = n.dir('data', 'DataPendaftaran')
+data_panitia_PTN2 = n.dir('data', 'DataPendaftaranPTN2')
+
+n.cmd('convert_data_panitia',
+    '${scriptdir}/convert-data-panitia.sh ${in} ${out} ${dlm}',
+    description='Convert CSV from Panitia ${in}'
+)
+
+n.cmd('union_data',
+    '${scriptdir}/union-data.sh ${in} ${out}',
+    description='Combine data ${in}'
+)
+
+arr = [
+    ('Data_Sekolah.csv', 'csv_sekolah.csv'),
+    ('Data_Siswa.csv', 'csv_siswa.csv'),
+    ('Data_Pilihan.csv', 'csv_pilihan.csv'),
+    ('Data_Jurusan.csv', 'csv_jurusan.csv'),
+    ('Ref_Jurusan.csv', 'csv_ref_jurusan.csv'),
+]
+
+unions = []
+for v in arr:
+    a = data_panitia_PTN1(v[0]).convert_data_panitia()
+    b = data_panitia_PTN2(v[0]).convert_data_panitia()
+    c = O([a, b], n).union_data(tmp(v[1]))
+    unions.append(c)
+
+n.generate()
---
+++ src/ninju.py
@@ -0,0 +1,200 @@
+import os
+import inspect
+
+import ninja_syntax
+from ninja_syntax import as_list
+
+class _NVar(object):
+    def __init__(self, name, value, indent=0):
+        super(_NVar, self).__init__()
+        self.name = name
+        self.value = value
+        self.indent = indent
+
+class _NPool(object):
+    def __init__(self, name, depth):
+        super(_NPool, self).__init__()
+        self.name = name
+        self.depth = depth
+
+    def write(self, writer):
+        writer.pool(self.name, self.depth)
+
+class _NRule(object):
+    def __init__(self, name, command, description=None, depfile=None,
+             generator=False, pool=None, restat=False, rspfile=None,
+             rspfile_content=None, deps=None):
+        super(_NRule, self).__init__()
+        self.name = name
+        self.command = command
+        self.description = description
+        self.depfile = depfile
+        self.generator = generator
+        self.pool = pool
+        self.restat = restat
+        self.rspfile = rspfile
+        self.rspfile_content = rspfile_content
+        self.deps = deps
+
+    def write(self, writer):
+        writer.rule(
+            self.name,
+            self.command,
+            self.description,
+            depfile=self.depfile,
+            generator=self.generator,
+            pool=self.pool,
+            restat=self.restat,
+            rspfile=self.rspfile,
+            rspfile_content=self.rspfile_content,
+            deps=self.deps)
+
+    def build_fn(self, ninju):
+        _self = self
+        def fn(inputs, outputs=None, implicit=None, order_only=None,
+                variables=None, implicit_outputs=None):
+            outs = _normalize_outputs(outputs, ninju._gen_name, 'build')
+            b = _NBuild(
+                    outs,
+                    _self.name,
+                    inputs=inputs,
+                    implicit=implicit,
+                    order_only=order_only,
+                    variables=variables,
+                    implicit_outputs=implicit_outputs)
+            ninju._seq.append(b)
+            return O(outs, ninju)
+        return fn
+
+class _NBuild(object):
+    def __init__(self, outputs, rule, inputs=None, implicit=None, order_only=None,
+            variables=None, implicit_outputs=None):
+        self.outputs = outputs
+        self.rule = rule
+        self.inputs = inputs
+        self.implicit = implicit
+        self.order_only = order_only
+        self.variables = variables
+        self.implicit_outputs = implicit_outputs
+
+    def write(self, writer):
+        outs = []
+        for o in as_list(self.outputs):
+            outs.append(str(o))
+
+        ins = []
+        for o in as_list(self.inputs):
+            ins.append(str(o))
+
+        writer.build(
+            outs,
+            self.rule,
+            inputs=ins,
+            implicit=self.implicit,
+            order_only=self.order_only,
+            variables=self.variables,
+            implicit_outputs=self.implicit_outputs)
+
+class O(object):
+
+    def __init__(self, paths, ninju):
+        super(O, self).__init__()
+        self.paths = paths
+        self._n = ninju
+
+    def __getattr__(self, name):
+        if name in self._n._commands:
+            cmd = self._n._commands[name]
+            p = self.paths
+            def fn(outputs=None, implicit=None, order_only=None,
+                    variables=None, implicit_outputs=None):
+                return cmd(p, outputs=outputs, implicit=implicit, order_only=order_only,
+                        variables=variables, implicit_outputs=implicit_outputs)
+            return fn
+        else:
+            raise AttributeError
+
+    def __repr__(self):
+        return self.paths.__repr__()
+
+    def __str__(self):
+        return self.paths.__str__()
+
+    def __bytes__(self):
+        return self.paths.__bytes__()
+
+    def __format__(self, format_spec):
+        return self.paths.__format__(format_spec)
+
+class Ninju(object):
+
+    """docstring for Ninju."""
+    def __init__(self, build_file='build.ninja', build_dir='.builddir'):
+        super(Ninju, self).__init__()
+        self._build_file = build_file
+        self._build_dir = build_dir
+        self._seq = []
+        self._name_count = 0
+        self._commands = {}
+
+        frame = inspect.stack()[1]
+        module = inspect.getmodule(frame[0])
+        if os.path.isabs(module.__file__):
+            self._root_dir = os.path.dirname(module.__file__)
+        else:
+            self._root_dir = os.path.dirname(os.path.abspath(module.__file__))
+
+    def dir(self, *args):
+        p = os.path.join('${NINJU_ROOT}', *args)
+        _self = self
+        def dirfn(*args):
+            return O(os.path.join(p, *args), _self)
+        return dirfn
+
+    def var(self, key, value):
+        v = _NVar(key, value)
+        self._seq.append(v)
+        return v
+
+    def cmd(self, name, command, description=None, depfile=None,
+             generator=False, pool=None, restat=False, rspfile=None,
+             rspfile_content=None, deps=None):
+        v = _NRule(
+                    name,
+                    command,
+                    description=description,
+                    depfile=depfile,
+                    generator=generator,
+                    pool=pool,
+                    restat=restat,
+                    rspfile=rspfile,
+                    rspfile_content=rspfile_content,
+                    deps=deps)
+        self._seq.append(v)
+        self._commands[name] = v.build_fn(self)
+
+    def execute(self):
+        pass
+
+    def _gen_name(self, suffix):
+        self._name_count += 1
+        return 'ninju_{suffix}_{index}'.format(suffix=suffix, index=self._name_count)
+
+    def generate(self, newline=True):
+        writer = ninja_syntax.Writer(open(os.path.join(self._root_dir, self._build_file), 'w'))
+        for task in self._seq:
+            task.write(writer)
+            if newline:
+                writer.newline()
+        writer.close()
+
+def _normalize_outputs(outputs, gen_name, suffix):
+    if not outputs:
+        return [gen_name(suffix)]
+    elif isinstance(outputs, int):
+        outs = []
+        for i in range(outputs):
+            outs.append(gen_name(suffix))
+        return outs
+    else:
+        return as_list(outputs)
